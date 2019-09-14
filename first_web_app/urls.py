32a4266db9f70004330916be9f1fd5875ac5896b
ilyana19 <aries_2152@hotmail.com>
2019-09-14 16:34:06 -0400

    added a redirect for gallery to portfolio

--- first_web_app/urls.py
+++ first_web_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from first_web_app.views import home_page, portfolio, about, favourites
+from first_web_app.views import root, home_page, gallery, portfolio, about, favourites
 
 urlpatterns = [
-    path('home/', home_page),
-    path('portfolio/', portfolio),
-    path('about_me/', about),
-    path('favourites/', favourites),
+  path('', root),
+  path('home/', home_page),
+  path('gallery/', gallery),
+  path('portfolio/', portfolio),
+  path('about_me/', about),
+  path('favourites/', favourites),
 ]
--- first_web_app/views.py
+++ first_web_app/views.py
@@ -1,12 +1,17 @@
 from random import randint
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+
+def root(request):
+  return HttpResponseRedirect('home')
 
 def home_page(request):
   context = {'name': 'Linda'}
   response = render(request, 'index.html', context)
   return HttpResponse(response)
 
+def gallery(request):
+  return HttpResponseRedirect('/portfolio')
 
 def portfolio(request):
   image_urls = []
@@ -19,7 +24,6 @@ def portfolio(request):
   response = render(request, 'gallery.html', context)
   return HttpResponse(response)
 
-
 def about(request):
   context = {
     'skills': ['ruby', 'rails', 'python'],
@@ -27,7 +31,6 @@ def about(request):
   }
   response = render(request, 'about.html', context)
   return HttpResponse(response)
-
 
 def favourites(request):
   context = {
